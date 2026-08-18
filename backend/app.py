866332e2c32806509d390676cd944a49662569cd
Dana-0655 <dhana1029384756@gmail.com>
2026-08-18 13:53:20 +0530

    grid added

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"connect_args": {"ssl": {"ca": "ca.pem"} }}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
